8b704b354fb28dcd41de648f5103b0a0cf754659
darek.filar <darek.filar@gmail.com>
2023-04-18 08:51:56 +0200

    Addded Account Controller

---
+++ SYSTEMATIC/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SYSTEMATIC.API.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
