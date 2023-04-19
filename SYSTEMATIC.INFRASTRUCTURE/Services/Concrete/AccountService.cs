46835e5ded92ac007b3631c6c73d924d7b0db950
darek.filar <darek.filar@gmail.com>
2023-04-19 13:23:20 +0200

    Changed dbcontext do userRepository in AccountService

--- SYSTEMATIC.INFRASTRUCTURE/Services/Concrete/AccountService.cs
+++ SYSTEMATIC.INFRASTRUCTURE/Services/Concrete/AccountService.cs
@@ -1,20 +1,19 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System.Security.Cryptography;
-using System.Text;
-using SYSTEMATIC.DB;
 using SYSTEMATIC.DB.Entities;
+using SYSTEMATIC.INFRASTRUCTURE.Repositories.Abstract;
 using SYSTEMATIC.INFRASTRUCTURE.Requests;
 
 namespace SYSTEMATIC.INFRASTRUCTURE.Services
 {
     public class AccountService : IAccountService
     {
-        private readonly SystematicDbContext _context;
+        private readonly IUserRepository _userRepository;
         private readonly IPasswordHasher<User> _passwordHasher;
-        public AccountService(SystematicDbContext context,
+        public AccountService(IUserRepository userRepository,
             IPasswordHasher<User> passwordHasher)
         {
-            _context = context;
+            _userRepository = userRepository;
             _passwordHasher = passwordHasher;
         }
         public async Task RegisterUserAsync(RegisterUserRequest request)
@@ -30,8 +29,7 @@ namespace SYSTEMATIC.INFRASTRUCTURE.Services
             newUser.PasswordHash = hashedPassword;
             newUser.PasswordSalt = salt;
 
-            _context.Users.Add(newUser);
-            await _context.SaveChangesAsync();
+            await _userRepository.AddAsync(newUser);
         }
 
         private static string GenerateSalt()
